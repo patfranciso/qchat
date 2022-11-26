93cb71e5a6047ea4c150e15da7db0f489826f901
Patrick Omogbeme <patfranciso@gmail.com>
2022-11-26 21:04:47 +0100

    Pages for the app

---
+++ src/pages/AuthPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex">
+    <h5>Auth Page</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'UsersPage',
+});
+</script>
---
+++ src/pages/UsersPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex">
+    <h5>Users Page</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'UsersPage',
+});
+</script>
