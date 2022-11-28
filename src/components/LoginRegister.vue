66c0f94b6e2af08f9eabb9f2940419a3d8a4556e
Patrick Omogbeme <patfranciso@gmail.com>
2022-11-29 00:15:47 +0100

    Auth page design done

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -1,7 +1,51 @@
 <template>
-  <h5>Login / Register {{ tab }}</h5>
+  <q-form class="q-gutter-md">
+    <q-input
+      v-if="tab == 'register'"
+      v-model="formData.name"
+      type="text"
+      class="q-mb-md"
+      outlined
+      label="Name"
+    />
+    <q-input
+      v-model="formData.email"
+      type="email"
+      class="q-mb-md"
+      outlined
+      label="Email"
+    />
+    <q-input
+      v-model="formData.password"
+      type="password"
+      class="q-mb-md"
+      outlined
+      label="Password"
+    />
+    <div class="row">
+      <q-space /><q-btn
+        color="primary"
+        :label="tab"
+        type="submit"
+        @click="submitForm"
+      />
+    </div>
+  </q-form>
 </template>
 <script setup>
+import { ref } from 'vue';
 const name = 'LoginRegister';
-defineProps(['tab']);
+const { tab } = defineProps(['tab']);
+let formData = ref({
+  name: '',
+  email: '',
+  password: '',
+});
+const submitForm = () => {
+  if (tab == 'login') {
+    console.log('login', formData.value);
+  } else {
+    console.log('register', formData.value);
+  }
+};
 </script>
