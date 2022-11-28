08bca5b3d256c6d395c18544138cf5adf467a6d5
Patrick Omogbeme <patfranciso@gmail.com>
2022-11-28 21:53:36 +0100

    Start message list

--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -1,5 +1,9 @@
 <template>
-  <q-page class="flex">
+  <q-page class="flex column">
+    <div class="q-pa-md column col justify-end">
+      <q-chat-message name="me" :text="['hey, how are you?']" sent />
+      <q-chat-message name="Jane" :text="['doing fine, how r you?']" />
+    </div>
     <q-footer elevated>
       <q-toolbar>
         <q-input
