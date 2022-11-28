80e5716dcd34679ead03a319d4851b74c5a7fb66
Patrick Omogbeme <patfranciso@gmail.com>
2022-11-28 18:41:25 +0100

    Chat view done

--- src/pages/UsersPage.vue
+++ src/pages/UsersPage.vue
@@ -1,52 +1,28 @@
 <template>
-  <q-page class="flex">
-    <q-list bordered>
+  <q-page class="flex q-pa-md">
+    <q-list class="full-width" separator>
       <q-item
-        v-for="contact in contacts"
-        :key="contact.id"
-        class="q-my-sm"
+        v-for="user in users"
+        to="/chat"
+        :key="user.id"
         clickable
         v-ripple
       >
         <q-item-section avatar>
           <q-avatar color="primary" text-color="white">
-            {{ contact.letter }}
+            {{ user.name.charAt(0) }}
           </q-avatar>
         </q-item-section>
 
         <q-item-section>
-          <q-item-label>{{ contact.name }}</q-item-label>
-          <q-item-label caption lines="1">{{ contact.email }}</q-item-label>
+          <q-item-label>{{ user.name }}</q-item-label>
+          <q-item-label caption lines="1">{{ user.email }}</q-item-label>
         </q-item-section>
 
         <q-item-section side>
-          <q-icon name="chat_bubble" color="green" />
-        </q-item-section>
-      </q-item>
-
-      <q-separator />
-      <q-item-label header>Offline</q-item-label>
-
-      <q-item
-        v-for="contact in offline"
-        :key="contact.id"
-        class="q-mb-sm"
-        clickable
-        v-ripple
-      >
-        <q-item-section avatar>
-          <q-avatar>
-            <img :src="`https://cdn.quasar.dev/img/${contact.avatar}`" />
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label>{{ contact.name }}</q-item-label>
-          <q-item-label caption lines="1">{{ contact.email }}</q-item-label>
-        </q-item-section>
-
-        <q-item-section side>
-          <q-icon name="chat_bubble" color="grey" />
+          <q-badge :color="user.online ? 'light-green-5' : 'grey-4'">
+            {{ user.online ? 'Online' : 'Offline' }}
+          </q-badge>
         </q-item-section>
       </q-item>
     </q-list>
@@ -55,44 +31,21 @@
 
 <script setup>
 name: 'UsersPage';
-const contacts = [
+const users = [
   {
     id: 1,
-    name: 'Ruddy Jedrzej',
-    email: 'rjedrzej0@discuz.net',
-    letter: 'R',
+    name: 'Pat',
+    online: true,
   },
   {
     id: 2,
     name: 'Mallorie Alessandrini',
-    email: 'malessandrini1@marketwatch.com',
-    letter: 'M',
+    online: false,
   },
   {
     id: 3,
     name: 'Elisabetta Wicklen',
-    email: 'ewicklen2@microsoft.com',
-    letter: 'E',
-  },
-  {
-    id: 4,
-    name: 'Seka Fawdrey',
-    email: 'sfawdrey3@wired.com',
-    letter: 'S',
-  },
-];
-const offline = [
-  {
-    id: 5,
-    name: 'Brunhilde Panswick',
-    email: 'bpanswick4@csmonitor.com',
-    avatar: 'avatar2.jpg',
-  },
-  {
-    id: 6,
-    name: 'Winfield Stapforth',
-    email: 'wstapforth5@pcworld.com',
-    avatar: 'avatar6.jpg',
+    online: true,
   },
 ];
 </script>
